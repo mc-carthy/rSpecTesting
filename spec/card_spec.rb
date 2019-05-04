2db5ff75ef1af15f64ff04d59a544ebddb6d65a7
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-05-04 22:45:51 +0100

    Refactor using let instead of before hook

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,5 @@
 class Card
-    attr_reader :rank, :suit
+    attr_accessor :rank, :suit
 
     def initialize(rank, suit)
         @rank = rank
@@ -8,14 +8,17 @@ class Card
 end
 
 RSpec.describe Card do
-    before do
-        @card = Card.new('Ace', 'Spades')
-    end
+    let(:card) { Card.new('Ace', 'Spades')}
 
     it 'has a rank' do
-        expect(@card.rank).to eq('Ace')
+        expect(card.rank).to eq('Ace')
     end
     it 'has a suit' do
-        expect(@card.suit).to eq('Spades')
+        expect(card.suit).to eq('Spades')
+    end
+    it 'has a modifiable rank' do
+        expect(card.rank).to eq('Ace')
+        card.rank = 'Queen'
+        expect(card.rank).to eq('Queen')
     end
 end
