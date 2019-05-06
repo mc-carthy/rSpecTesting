ef66eb1ea8c8eb321e6355ac06b86b60e7a0acbc
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-05-06 22:05:34 +0100

    Additional examples for implicit subjects

--- spec/implicit_subject_spec.rb
+++ spec/implicit_subject_spec.rb
@@ -1,5 +1,11 @@
 RSpec.describe Hash do
     it 'should start empty' do
         expect(subject.length).to eq(0)
+        subject[:test_key] = 'Test value'
+        expect(subject.length).to eq(1)
+    end
+    
+    it 'should not be persisted between examples' do
+        expect(subject.length).to eq(0)
     end
 end
